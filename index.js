4aefbc94e0fc7cedd7c8efb433175b507d461cb6
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-04-15 19:00:45 +0200

    CORS

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var request = require('request');
 
 var app = express();	
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use('/', function(req, res) {
 	var url = req.url.substr(1);
 
