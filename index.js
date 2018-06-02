87708b06e3a25eb102f54a3a52a85e452d422632
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-02 19:15:34 +0200

    POST

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 
 var app = express();	
+
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -12,17 +15,32 @@ app.use(function(req, res, next) {
 app.use('/', function(req, res) {
 	var url = req.url.substr(1);
 
-	try {
-		var stream = request({
-			followAllRedirects: true,
-			url: url
-		});
+	delete req.headers.host;
 
-		stream
-			.on('error', function(err) {
-				res.send("Error");
-			})
-			.pipe(res);
+	let options = {
+		headers: req.headers,
+		followAllRedirects: true,
+		url: url
+	};
+
+	if (req.method == "POST") {
+		options.method = "POST";
+		options.body = JSON.stringify(req.body);
+	} else if(req.method == "OPTIONS"){
+		res.send("POST");
+		return;
+	}
+
+	try {
+		request(options, function (error, response, body) {
+			Object.keys(response.headers).forEach((key) => {
+				if(key.indexOf("access-control") === -1){
+					res.set(key, response.headers[key]);
+				}
+			});
+
+			res.send(body);
+		});
 	} catch (e) {
 		res.send("Error");
 	}
